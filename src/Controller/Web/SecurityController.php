4e6c506243565bad3ea95e603d967e1f472c1116
maihe <maihe@howzhi.com>
2018-03-09 17:13:36 +0800

    fix

---
+++ src/Controller/Web/SecurityController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\Web;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function index(Request $request, AuthenticationUtils $authUtils)
+    {
+        $error = $authUtils->getLastAuthenticationError();
+        $lastUsername = $authUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', array(
+            'error' => $error,
+            'lastUsername' => $lastUsername
+        ));
+    }
+}
