99d8f1723f13f3bb135ad0d14fc103bad5a47d19
maihe <maihe@howzhi.com>
2018-03-30 13:56:32 +0800

    fron end

--- webpack.config.js
+++ webpack.config.js
@@ -8,15 +8,23 @@ Encore
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
     // uncomment to create hashed filenames (e.g. app.abc123.css)
-    // .enableVersioning(Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
 
     // uncomment to define the assets of the project
-    // .addEntry('js/app', './assets/js/app.js')
-    // .addStyleEntry('css/app', './assets/css/app.scss')
+    .addEntry('js/main', './assets/js/main.js')
+    .addStyleEntry('css/main', './assets/css/main.less')
 
     // uncomment if you use Sass/SCSS files
-    // .enableSassLoader()
+    .enableLessLoader()
+    .enableVueLoader()
 
+    .configureBabel(function(babelConfig) {
+      // add additional presets
+      babelConfig.presets.push('es2017');
+
+      // no plugins are added by default, but you can add some
+      // babelConfig.plugins.push('styled-jsx/babel');
+    })
     // uncomment for legacy applications that require $/jQuery as a global variable
     // .autoProvidejQuery()
 ;
